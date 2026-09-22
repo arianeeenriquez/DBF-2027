e66ae98eaf9063bfcca3cdedb9655915e6bc93b0
arianeeenriquez <arianee@mit.edu>
2026-09-22 22:33:24 +0000

    Added GM (ish. not really)

--- plane_project/static_functions/mission.py
+++ plane_project/static_functions/mission.py
@@ -1,14 +1,19 @@
 def M2(v):
 	v.M2_score = v.mass_sensor_total / v.M2_time
 	return v.M2_score
-	
 
-def M3(v): 
+
+def M3(v):
 	v.M3_laps = v.velocity_M3 * 5 * 60 / v.lap_dist
 	v.M3_score = v.mass_sensor / v.M3_laps
 	return v.M3_score
-	
+
+# def GM(v):
+# 	v.GM_weight =
+# 	v.GM_height =
+# 	v.GM_score = + v.GM_weight * v.GM_height
+# 	return v.GM_score
 
 def both_missions(v):
-	v.both_mission_score = M2(v) / v.M2_max + M3(v) / v.M3_max
+	v.both_mission_score = 3 + M2(v) / v.M2_max + M3(v) / v.M3_max
 	return v.both_mission_score
